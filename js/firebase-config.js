9c2fe4e6e84677109aeb9fdc0865566bae92e273
stiacademy <stiacademy346@gmail.com>
2026-09-23 11:43:30 +0100

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHa_irkP8QFgCQjUqTKlqdmv5cf8Ec4hg",
+    authDomain: "sti-academy.firebaseapp.com",
+    projectId: "sti-academy",
+    storageBucket: "sti-academy.firebasestorage.app",
+    messagingSenderId: "709840641262",
+    appId: "1:709840641262:web:714b09bf661afa159f27ff",
+    measurementId: "G-7ZRZ3P28YP"
+};
 
+firebase.initializeApp(firebaseConfig);
